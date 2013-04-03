1672e6b96a2997b619970b393ca10c292ef67774
Michael Locher <cmbntr@gmail.com>
2013-04-03 23:44:33 +0200

    fix shading

---
+++ ten-data/ten-data-persistent/src/main/java/ch/cmbntr/ten/data/persistent/impl/MinimizerKeepAlive.java
@@ -0,0 +1,13 @@
+package ch.cmbntr.ten.data.persistent.impl;
+
+class MinimizerKeepAlive {
+
+  public static final Class<?>[] KEEP = { clojure.core__init.class, clojure.core_deftype__init.class,
+      clojure.core_print__init.class, clojure.core_proxy__init.class, clojure.data__init.class,
+      clojure.edn__init.class, clojure.genclass__init.class, clojure.gvec__init.class, clojure.instant__init.class,
+      clojure.main__init.class, clojure.pprint__init.class, clojure.reflect__init.class, clojure.repl__init.class,
+      clojure.set__init.class, clojure.stacktrace__init.class, clojure.string__init.class,
+      clojure.template__init.class, clojure.uuid__init.class, clojure.walk__init.class, clojure.xml__init.class,
+      clojure.zip__init.class, clojure.core.protocols__init.class, clojure.java.io__init.class };
+
+}
